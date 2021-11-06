c2234d4dcae0d13087143365129c5cb53bb70b79
Vipul <singhvipul9798@gmail.com>
2021-11-06 18:58:54 +0530

    new file to add

---
+++ oops.py
@@ -0,0 +1,31 @@
+# class student:
+#     def __init__(self, name, major, gpa , is_on_probation):
+#         self.name = name
+#         self.major = major
+#         self.gpa = gpa
+#         self.is_on_probation = is_on_probation
+
+
+
+# student1 = student("Vipul","computer science", 9.5, False)
+# print(student1.name)
+
+
+
+
+class Chef:
+
+
+    def make_chicken(self):
+        print("The chef makes the chicken")
+    def make_salad(self):
+        print("The chef makes the salad")
+    def make_special_dish(self):
+        print("The chef makes the special dish")
+
+
+
+
+
+    myChef = Chef()
+    myChef.make_chicken()
